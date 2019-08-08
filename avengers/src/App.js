c280eaeab5271c773e5e0e66e5203a986962ba37
meholt <meholt90@gmail.com>
2019-08-07 21:51:21 -0700

    trying-to-get-nav/router-component-to-work

--- avengers/src/App.js
+++ avengers/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import { Navigation } from '../../src/components';
 
 function App() {
   return (
     <div className="App">
-      
+
+{/* * Create 3 `<Route />` setting their `path` prop equal to `/`, `/about`, `/contact` with their respective components.
+* Be sure to incluce the `exact` prop on the root component for `/` to make sure that it's rendering the exact component and not all the other components. */}
+
+      <Navigation />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/Contact" component={Contact}/>
+
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
+
+    // * Declare the `to` as the href on `<Link>` 
+    // and specify the correct routes for your app to navigate towards.
+
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="./home">Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="./about">About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="./contact">Contact</Link>
         </div>
       </div>
     </div>
